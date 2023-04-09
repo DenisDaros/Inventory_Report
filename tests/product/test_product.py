a90d65fb0f2be447dbe7207e6c4bb4bc9b43c83a
denis daros <denisv.daros@gmail.com>
2023-04-09 18:20:24 -0300

    implementando teste da classe product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "chocolate",
+        "Willie Wonka",
+        "01/01/2022",
+        "01/01/2023",
+        "1234",
+        "estocado",
+    )
+    assert produto.id == 1
+    assert produto.nome_da_empresa == "Willie Wonka"
+    assert produto.nome_do_produto == "chocolate"
+    assert produto.data_de_fabricacao == "01/01/2022"
+    assert produto.data_de_validade == "01/01/2023"
+    assert produto.numero_de_serie == "1234"
+    assert produto.instrucoes_de_armazenamento == "estocado"
