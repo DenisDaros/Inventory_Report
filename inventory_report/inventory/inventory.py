802522cb520ad24c0c90167b556622c7ab3c6a70
denis daros <denisv.daros@gmail.com>
2023-04-11 15:37:17 -0300

    gerando relátorios de arquivos JSON sobrescrevendo o método import_data

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,30 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, type):
+    def readCsv(cls, path, type):
         with open(path) as file:
             if type == "simples":
                 return SimpleReport.generate(list(csv.DictReader(file)))
-
             if type == "completo":
                 return CompleteReport.generate(list(csv.DictReader(file)))
+
+    @classmethod
+    def readJson(cls, path, type):
+        with open(path) as fileJson:
+            if type == "simples":
+                return SimpleReport.generate(json.load(fileJson))
+            if type == "completo":
+                return CompleteReport.generate(json.load(fileJson))
+
+    @classmethod
+    def import_data(cls, path, type):
+        if ".csv" in path:
+            return Inventory.readCsv(path, type)
+
+        if ".json" in path:
+            return Inventory.readJson(path, type)
